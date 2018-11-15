e37bc645949b8bdd942e195e2715f775df21d646
Yasir Akbar <yasir.akbar86@hotmail.com>
2018-11-15 15:33:17 +0200

    small fix

--- src/utils/AuthService.js
+++ src/utils/AuthService.js
@@ -1,6 +1,8 @@
 import decode from 'jwt-decode';
 import { browserHistory } from 'react-router';
 import auth0 from 'auth0-js';
+
+
 const ID_TOKEN_KEY = 'id_token';
 const ACCESS_TOKEN_KEY = 'access_token';
 
